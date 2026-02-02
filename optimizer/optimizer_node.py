81bc5d7f19e375b63871e9c5e48116ef8b892d6a
VaishnaviW10 <130399033+VaishnaviW10@users.noreply.github.com>
2026-02-02 23:12:50 +0530

    Refactor optimizer_node.py to streamline LLM calls

--- optimizer/optimizer_node.py
+++ optimizer/optimizer_node.py
@@ -8,46 +8,9 @@ from langchain_ollama import OllamaLLM
 llm = OllamaLLM(
     model="phi3:mini",
     temperature=0.0,
-    max_tokens=50,  # IMPORTANT
+    max_tokens=50, 
     keep_alive="5m"
 )
-
-
-# Local LLM running via Ollama
-#llm = Ollama(model="mistral")
-'''
-def optimize_prompt(state: dict) -> dict:
-    user_prompt = state["user_prompt"]
-
-    optimized_prompt = llm.invoke(
-        f"{OPTIMIZER_SYSTEM_PROMPT}\n\nUser Query:\n{user_prompt}"
-    ).strip()
-
-    return {
-        "original_prompt": user_prompt,
-        "optimized_prompt": optimized_prompt,
-        "original_tokens": count_tokens(user_prompt),
-        "optimized_tokens": count_tokens(optimized_prompt),
-    }
-'''
-'''
-def optimize_prompt(state: dict) -> dict:
-    user_prompt = state["user_prompt"]
-
-    optimized_prompt = llm.invoke(
-        f"{OPTIMIZER_SYSTEM_PROMPT}\n\nUser Query:\n{user_prompt}"
-    )
-
-    optimized_prompt = optimized_prompt.strip().split("\n")[0]
-
-    return {
-        "original_prompt": user_prompt,
-        "optimized_prompt": optimized_prompt,
-        "original_tokens": count_tokens(user_prompt),
-        "optimized_tokens": count_tokens(optimized_prompt),
-    }
-'''
-
 
 def optimize_prompt(state: dict) -> dict:
     """
@@ -63,3 +26,4 @@ def optimize_prompt(state: dict) -> dict:
     final_response = llm.invoke(optimized_query)
 
     return {"final_response": final_response}
+
